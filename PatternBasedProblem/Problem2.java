9dbc296ca38003c486fb8dafbcabb5fee9d4437d
mjbharathgithub <mjbharath2003@gmail.com>
2024-04-24 12:42:42 +0530

    UPDATE PROBLEM 2

--- PatternBasedProblem/Problem2.java
+++ PatternBasedProblem/Problem2.java
@@ -1,6 +1,6 @@
 package PatternBasedProblem;
 import java.util.*;
-class Main{
+class Problem2{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
