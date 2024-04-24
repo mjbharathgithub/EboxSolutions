9656d1723f725ea7e005da65c48f0ad1614c286a
mjbharathgithub <mjbharath2003@gmail.com>
2024-04-24 12:38:07 +0530

    COMMIT READY TO PUSH PROBLEM 2

---
+++ PatternBasedProblem/Problem2.java
@@ -0,0 +1,28 @@
+package PatternBasedProblem;
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int row=0,col=1;
+        for(row=1;row<=n*n;row++){
+            if(row<=n|| row>(n*n-n) || ( row>(n/2*n)&&row<=(n/2*n)+n  )) {
+                if(col==n){
+                    System.out.println(0);
+                    col=0;
+                }
+                else System.out.print(1);
+
+            }
+            else{
+                if(col==1)System.out.print(1);
+                else if(col==n){
+                    System.out.println(1);
+                    col=0;
+            }
+           else System.out.print(0);
+            }
+             col++;
+        }
+    }
+}
